97739086114c3681fb43538ce44873020a799ee2
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-13 14:16:01 +0200

    Adding class RenameArea

---
+++ src/main/java/fr/pederobien/minecraftarea/commands/area/RenameArea.java
@@ -0,0 +1,28 @@
+package fr.pederobien.minecraftarea.commands.area;
+
+import org.bukkit.command.CommandSender;
+
+import fr.pederobien.minecraftarea.interfaces.IArea;
+import fr.pederobien.minecraftgameplateform.commands.common.CommonRename;
+
+public class RenameArea extends CommonRename<IArea> {
+
+	protected RenameArea() {
+		super(EAreaMessageCode.RENAME_AREA__EXPLANATION);
+	}
+
+	@Override
+	protected void onNameAlreadyTaken(CommandSender sender, String currentName, String newName) {
+		sendMessageToSender(sender, EAreaMessageCode.RENAME_AREA__NAME_ALREADY_TAKEN, currentName, newName);
+	}
+
+	@Override
+	protected void onNameIsMissing(CommandSender sender, String oldName) {
+		sendMessageToSender(sender, EAreaMessageCode.RENAME_AREA__NAME_IS_MISSING, oldName);
+	}
+
+	@Override
+	protected void onRenamed(CommandSender sender, String oldName, String newName) {
+		sendMessageToSender(sender, EAreaMessageCode.RENAME_AREA__AREA_RENAMED, oldName, newName);
+	}
+}
