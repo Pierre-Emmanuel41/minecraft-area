44605c737b2dd2a425e1f8423f167313387d65c6
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-13 15:40:30 +0200

    Adding class SaveArea

---
+++ src/main/java/fr/pederobien/minecraftarea/commands/area/SaveArea.java
@@ -0,0 +1,18 @@
+package fr.pederobien.minecraftarea.commands.area;
+
+import org.bukkit.command.CommandSender;
+
+import fr.pederobien.minecraftarea.interfaces.IArea;
+import fr.pederobien.minecraftgameplateform.commands.common.CommonSave;
+
+public class SaveArea extends CommonSave<IArea> {
+
+	protected SaveArea() {
+		super(EAreaMessageCode.SAVE_AREA__EXPLANATION);
+	}
+
+	@Override
+	protected void onSave(CommandSender sender, String name) {
+		sendMessageToSender(sender, EAreaMessageCode.SAVE_AREA__AREA_SAVED, name);
+	}
+}
