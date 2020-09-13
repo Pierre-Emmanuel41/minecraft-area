81b439a29c7d3498efc6edbfbf1ee637bfd7c756
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-13 15:57:00 +0200

    Adding class DeleteArea

---
+++ src/main/java/fr/pederobien/minecraftarea/commands/area/DeleteArea.java
@@ -0,0 +1,28 @@
+package fr.pederobien.minecraftarea.commands.area;
+
+import org.bukkit.command.CommandSender;
+
+import fr.pederobien.minecraftarea.interfaces.IArea;
+import fr.pederobien.minecraftgameplateform.commands.common.CommonDelete;
+
+public class DeleteArea extends CommonDelete<IArea> {
+
+	protected DeleteArea() {
+		super(EAreaMessageCode.DELETE_AREA__EXPLANATION);
+	}
+
+	@Override
+	protected void onDidNotDelete(CommandSender sender, String name) {
+		sendMessageToSender(sender, EAreaMessageCode.DELETE_AREA__DIT_NOT_DELETE, name);
+	}
+
+	@Override
+	protected void onDeleted(CommandSender sender, String name) {
+		sendMessageToSender(sender, EAreaMessageCode.DELETE_AREA__AREA_DELETED, name);
+	}
+
+	@Override
+	protected void onNameIsMissing(CommandSender sender) {
+		sendMessageToSender(sender, EAreaMessageCode.DELETE_AREA__NAME_IS_MISSING);
+	}
+}
