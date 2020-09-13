e97ae2671790be896e75b19afad4daa5de349d38
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-13 16:02:00 +0200

    Adding class ExtractArea

---
+++ src/main/java/fr/pederobien/minecraftarea/commands/area/ExtractArea.java
@@ -0,0 +1,18 @@
+package fr.pederobien.minecraftarea.commands.area;
+
+import org.bukkit.command.CommandSender;
+
+import fr.pederobien.minecraftarea.commands.CommonExtract;
+import fr.pederobien.minecraftarea.interfaces.IArea;
+
+public class ExtractArea extends CommonExtract<IArea> {
+
+	protected ExtractArea() {
+		super(EAreaMessageCode.EXTRACT_AREA__EXPLANATION);
+	}
+
+	@Override
+	protected void onExtracted(CommandSender sender, String name) {
+		sendMessageToSender(sender, EAreaMessageCode.EXTRACT_AREA__AREA_EXTRACTED, name);
+	}
+}
