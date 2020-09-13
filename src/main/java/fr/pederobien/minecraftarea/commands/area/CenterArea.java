a6d5feb97dce83967b31d6a197c22731e0ba9f4c
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2020-09-13 14:00:31 +0200

    Adding class CenterArea

---
+++ src/main/java/fr/pederobien/minecraftarea/commands/area/CenterArea.java
@@ -0,0 +1,18 @@
+package fr.pederobien.minecraftarea.commands.area;
+
+import org.bukkit.command.CommandSender;
+
+import fr.pederobien.minecraftarea.commands.CommonCenter;
+import fr.pederobien.minecraftarea.interfaces.IArea;
+
+public class CenterArea extends CommonCenter<IArea> {
+
+	public CenterArea() {
+		super(EAreaMessageCode.CENTER_AREA__EXPLANATION);
+	}
+
+	@Override
+	protected void onCenterDefined(CommandSender sender, String name, int x, int y, int z) {
+		sendMessageToSender(sender, EAreaMessageCode.CENTER_AREA__CENTER_DEFINED, name, x, y, z);
+	}
+}
